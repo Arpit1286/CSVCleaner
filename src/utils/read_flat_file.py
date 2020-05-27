5c26efa30aadbd2aaa6e7e34ee71eb4c287e4f72
Arpit1286 <arpit.tandon@ymail.com>
2020-05-27 09:23:58 -0700

    delimiter method added

--- src/utils/read_flat_file.py
+++ src/utils/read_flat_file.py
@@ -14,21 +14,34 @@ LOGGER = logging.getLogger()
 class ReadFile(ReadUtils):
 
     def check_path(self, path):
+        """
+        check if the path exists, if not gracefully exit
+        :param path: input file path
+        :return:
+        """
         if os.path.isfile(path):
             LOGGER.info("Reading file from %s" % path)
             return True
         else:
             LOGGER.error("File / path not present")
+            sys.exit(-1)
             return False
 
-    # TODO: write the get delimiter function
+    # using csv Sniffer to get delimiter
     def get_delimiter(self, path):
+        """
+        infer the delimiter using the csv sniffer
+        :param path:
+        :return:
+        """
         boolean = self.check_path(path)
         dialect = None
         if boolean:
-            with open(path, newline='') as csvfile:
-                dialect = csv.Sniffer().sniff(csvfile.read(1024))
-            return dialect
+            try:
+                with open(path, newline='') as csv_file:
+                    dialect = csv.Sniffer().sniff(csv_file.read(1024))
+                return dialect.delimiter
+            except error as e
         return "not a valid path"
 
     # TODO: modify to get the delimiter from the file and use it in this
