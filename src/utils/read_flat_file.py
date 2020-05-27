e6de7c205a4dac162aa81afccc5f70e56d2d18bb
Arpit1286 <arpit.tandon@ymail.com>
2020-05-27 01:16:21 -0700

    example file added

--- src/utils/read_flat_file.py
+++ src/utils/read_flat_file.py
@@ -4,6 +4,7 @@
 import os
 import logging
 import pandas as pd
+import csv
 
 from src.utils.read_util import ReadUtils
 
@@ -21,7 +22,14 @@ class ReadFile(ReadUtils):
             return False
 
     # TODO: write the get delimiter function
-
+    def get_delimiter(self, path):
+        boolean = self.check_path(path)
+        dialect = None
+        if boolean:
+            with open(path, newline='') as csvfile:
+                dialect = csv.Sniffer().sniff(csvfile.read(1024))
+            return dialect
+        return "not a valid path"
 
     # TODO: modify to get the delimiter from the file and use it in this
     def read_file(self, path):
@@ -30,7 +38,3 @@ class ReadFile(ReadUtils):
         if boolean:
             df = pd.read_csv(path)
         return df
-
-
-
-
