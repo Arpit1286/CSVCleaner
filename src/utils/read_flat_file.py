2c4579d0f2b0ec85084a31e11412d12c336ccdef
Arpit1286 <arpit.tandon@ymail.com>
2020-05-26 22:03:52 -0700

    first commit for file read method

---
+++ src/utils/read_flat_file.py
@@ -0,0 +1,36 @@
+# Author: Arpit Tandon
+# Read utilities for files
+
+import os
+import logging
+import pandas as pd
+
+from src.utils.read_util import ReadUtils
+
+LOGGER = logging.getLogger()
+
+
+class ReadFile(ReadUtils):
+
+    def check_path(self, path):
+        if os.path.isfile(path):
+            LOGGER.info("Reading file from %s" % path)
+            return True
+        else:
+            LOGGER.error("File / path not present")
+            return False
+
+    # TODO: write the get delimiter function
+
+
+    # TODO: modify to get the delimiter from the file and use it in this
+    def read_file(self, path):
+        boolean = self.check_path(path)
+        df = None
+        if boolean:
+            df = pd.read_csv(path)
+        return df
+
+
+
+
---
+++ src/utils/read_util.py
@@ -0,0 +1,67 @@
+# Author: Arpit Tandon
+# Read Utilities base class
+
+
+class ReadUtils:
+
+    def check_path(self):
+        """
+        check if the path entered is correct
+        :return:
+        """
+        pass
+
+    def get_delimiter(self):
+        """
+        return the delimiter for the file
+        :return:
+        """
+
+    def read_file(self):
+        """
+        read the file into a dataframe
+        :return:
+        """
+        pass
+
+    def get_header(self):
+        """
+        get the header from the dataframe
+        :return:
+        """
+        pass
+
+    def get_schema(self):
+        """
+        get the column schema from the dataframe
+        :return:
+        """
+        pass
+
+    def get_column_type(self):
+        """
+        get the inferred column type from dataframe
+        :return:
+        """
+        pass
+
+    def get_column_number(self):
+        """
+        get the column position for iloc operations
+        :return:
+        """
+        pass
+
+    def get_column_values_list(self):
+        """
+        get the columns values as list
+        :return:
+        """
+        pass
+
+    def get_column_values_series(self):
+        """
+        get the column values as np series
+        :return:
+        """
+        pass
